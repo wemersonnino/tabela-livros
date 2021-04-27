ad7b8d6d8cb86c96c996d7116d8d9475ad6fa7b7
Wemerson Nino <wemersonnino@gmail.com>
2021-04-27 00:55:31 -0300

    chamada fetch para lista de livros contidas em json file

--- src/App.js
+++ src/App.js
@@ -6,24 +6,20 @@ import TableFoot from "./components/TableFoot";
 
 class App extends Component{
     state = {
-        livros: [
-            {
-                id:"978-85-7522-632-2",
-                titulo: "CSS Grid Layout",
-                autor: "Mauricio Samy Silva"
-            },
-            {
-                id:"978-85-7522-677-3",
-                titulo: "Node Essencial",
-                autor: "Ricardo R. Lacheta"
-            },
-            {
-                id:"978-85-7522-512-7",
-                titulo: "Aprendendo Material Design",
-                autor: "Kyle Mew"
-            }
-        ]
+        livros: []
+    };
+    componentDidMount() {
+        fetch("/api/livros.json")
+            .then(response => response.json())
+            .then(livros => this.setState({livros}))
+            .catch(function (error){
+                console.log("Erro na requisição")
+            })
+            .finally(function () {
+                console.log("sempre retorna")
+            });
     }
+
     render() {
         return(
             <table className="tabela">
